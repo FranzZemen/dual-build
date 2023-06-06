5e824e2de81be35e2136204a8ad51685edc554b0
Franz Zemen <franzzemen@gmail.com>
2023-06-06 08:08:06 -0400

    testing

--- src/project/lib/transform/change-working-directory.transform.ts
+++ src/project/lib/transform/change-working-directory.transform.ts
@@ -6,32 +6,38 @@ License Type:
 import {resolve} from 'node:path';
 import {chdir, cwd} from 'node:process';
 import {processUnknownError} from '../../util/index.js';
-import {TransformIn} from '../../transform/index.js';
+import {TransformPayloadIn} from '../../transform/index.js';
 
 
-export class ChangeWorkingDirectory extends TransformIn<string> {
+export class ChangeWorkingDirectory extends TransformPayloadIn<string | undefined, string | undefined> {
   constructor(logDepth: number) {
     super(logDepth);
   }
 
-  executeImplIn(rootDirectorPath: string): Promise<void> {
-    if (rootDirectorPath) {
-      try {
-        const newCwd = rootDirectorPath;
-        this.contextLog.info(`current working directory is ${cwd()}`, 'task-internal')
-        const newCwdPath = resolve(cwd(), newCwd);
-        chdir(newCwdPath);
-        this.contextLog.info(`new working directory is ${cwd()}`, 'warn');
-        return Promise.resolve();
-      } catch (err) {
-        return Promise.reject(processUnknownError(err, this.contextLog));
-      }
+  public transformContext(rootDirectoryPath: string): string {
+    return rootDirectoryPath;
+  }
+
+  protected executeImplPayloadIn(pipeIn: string | undefined, passedIn: string | undefined): Promise<void> {
+    if (pipeIn) {
+      return this.changeWorkingDirectory(pipeIn);
+    } else if(passedIn) {
+      return this.changeWorkingDirectory(passedIn);
     } else {
       throw new Error('Undefined payload');
     }
   }
 
-  public transformContext(rootDirectoryPath: string): string {
-    return rootDirectoryPath;
+  private changeWorkingDirectory(rootDirectorPath: string): Promise<void> {
+    try {
+      const newCwd = rootDirectorPath;
+      this.contextLog.info(`current working directory is ${cwd()}`, 'task-internal')
+      const newCwdPath = resolve(cwd(), newCwd);
+      chdir(newCwdPath);
+      this.contextLog.info(`new working directory is ${cwd()}`, 'warn');
+      return Promise.resolve();
+    } catch (err) {
+      return Promise.reject(processUnknownError(err, this.contextLog));
+    }
   }
 }
